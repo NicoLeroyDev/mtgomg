59913a4a32fce3a35b66e810912938c8a9a6fc00
Nicolas <nleroy@novius.fr>
2021-08-17 14:25:16 +0200

    feature/route-to-collection-converter

---
+++ app/Http/Controllers/CollectionConverter.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Application;
+use Inertia\Inertia;
+
+class CollectionConverter extends Controller
+{
+    public function show()
+    {
+        Inertia::render('CollectionConverter', [
+            'canLogin' => Route::has('login'),
+            'canRegister' => Route::has('register'),
+            'laravelVersion' => Application::VERSION,
+            'phpVersion' => PHP_VERSION,
+        ]);
+    }
+}
