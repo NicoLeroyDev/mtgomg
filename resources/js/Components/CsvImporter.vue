381df51ff4eaaa04ded26c189364a0c8de6d3e99
Arakkus <nicoleroymultimedia@gmail.com>
2021-12-03 16:22:15 +0100

    fix/vue-csv-import-js-error

--- resources/js/Components/CsvImporter.vue
+++ resources/js/Components/CsvImporter.vue
@@ -6,38 +6,9 @@
         >
             <vue-csv-toggle-headers></vue-csv-toggle-headers>
             <vue-csv-errors></vue-csv-errors>
-            <vue-csv-input name="file"></vue-csv-input>
-            <vue-csv-map v-slot="{sample, map, fields}" :table-attributes="tableAttributes">
-                <table v-bind="$attrs" v-if="sample">
-                    <thead>
-                        <tr>
-                            <th>Field</th>
-                            <th>Column</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="(field, key) in fields" :key="key">
-                            <td>
-                                {{ field.label }}
-                                <span v-if="field.required">*</span>
-                            </td>
-                            <td>
-                                <select :name="`csv_uploader_map_${key}`"
-                                        v-model="map[field.key]"
-                                >
-                                    <option v-if="!field.required" :value="null">&nbsp;</option>
-                                    <option v-for="(column, key) in sample"
-                                            :key="key"
-                                            :value="key"
-                                    >
-                                        {{ column }}
-                                    </option>
-                                </select>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </vue-csv-map>
+            <vue-csv-input name="file">
+                <input ref="csv" type="file" @change="customChange" :name="name" v-bind="$attrs">
+            </vue-csv-input>
             <button @click.prevent="readCsv">Go!</button>
         </vue-csv-import>
         <div>{{ csv }}</div>
@@ -45,7 +16,7 @@
 </template>
 
 <script>
-    import { defineComponent, ref } from 'vue';
+    import { defineComponent, ref, inject } from 'vue';
     import { VueCsvToggleHeaders, VueCsvSubmit, VueCsvMap, VueCsvInput, VueCsvErrors, VueCsvImport } from 'vue-csv-import';
 
     export default defineComponent({
@@ -70,16 +41,34 @@
         ],
 
         setup(props, { emit }) {
+            console.log('1');
+
             const csv = ref(null);
             const fields = props.fields;
+            const VueCsvImportData = inject('VueCsvImportData');
 
             const readCsv = () => {
                 emit('readCsv', csv);
             }
 
+            const customChange = function () {
+                console.log("dfdd");
+                if (!csv || !csv.value) {
+                    //VueCsvImportData.file = null;
+                    return;
+                }
+
+                let tmpFile = csv.value.files ? csv.value.files[0] : null;
+                if (validate(tmpFile)) {
+                    VueCsvImportData.file = tmpFile;
+                }
+            }
+
+
             return {
                 readCsv,
                 csv,
+                customChange,
             }
         }
     })
