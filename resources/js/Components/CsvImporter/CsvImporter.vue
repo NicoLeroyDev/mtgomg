bb033d1d96022ff4f132f06f2dda406f869ec535
Nicolas Leroy <nicoleroymultimedia@gmail.com>
2022-03-08 17:41:31 +0100

    feature/csv-importer

---
+++ resources/js/Components/CsvImporter/CsvImporter.vue
@@ -0,0 +1,17 @@
+<template>
+  <csv-input />
+</template>
+
+<script>
+import CsvInput from "./CsvInput.vue";
+
+export default {
+  components: {
+    CsvInput,
+  },
+
+  data() {
+    return {};
+  },
+};
+</script>
---
+++ resources/js/Components/CsvImporter/CsvInput.vue
@@ -0,0 +1,72 @@
+<template>
+  <label class="text-reader">
+    Read File
+    <input type="file" @change="checkFileExtension" />
+  </label>
+</template>
+
+<script>
+export default {
+  methods: {
+    checkFileExtension(e) {
+      const files = e.target.files || e.dataTransfer.files;
+      const acceptedFileTypes = ["text/csv"];
+
+      if (files.length && acceptedFileTypes.includes(files[0].type)) {
+        this.readFile(e);
+      } else {
+        // Error logic
+      }
+    },
+
+    readFile(evt) {
+      const scope = this;
+      const promiseVar = new Promise(async (resolve, reject) => {
+        try {
+          const reader = new FileReader();
+          const file = evt.target.files[0];
+          reader.readAsText(file, "UTF-8");
+          reader.onload = (e) => {
+            scope.fileinput = e.target.result;
+            resolve("complete");
+          };
+        } catch (error) {
+          console.error(error);
+          reject("There must be an error");
+        }
+      });
+      // let updateds = await Promise.all(promiseVar);
+      promiseVar.then((data) => {
+        scope.afterRead();
+      });
+    },
+
+    afterRead() {
+      const result = [];
+      const lines = this.fileinput.split("\n");
+
+      // CSV headers are mandatory
+      const headers = lines[0].split(",");
+
+      lines.forEach((line) => {
+        if (line) {
+          const obj = {};
+          const currentLine = line.split(",");
+
+          headers.forEach((header, index) => {
+            obj[index] = currentLine[index];
+          });
+
+          result.push(obj);
+        }
+      });
+
+      console.log(result);
+    },
+  },
+
+  data() {
+    return {};
+  },
+};
+</script>
