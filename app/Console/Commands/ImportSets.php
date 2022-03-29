5ee7aa7bb593e97411fbae47b9d5f9c1a31e0b89
Nicolas Leroy <nicoleroymultimedia@gmail.com>
2022-03-29 17:17:42 +0200

    feature/card-model wip

---
+++ app/Console/Commands/ImportSets.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Http\Controllers\SetController;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+
+class ImportSets extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'import:sets';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Import sets from Scryfall API';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $response = Http::get(env('SCRYFALL_API_URL') . '/sets/');
+
+        if ($response->status() !== 200) {
+            $this->error('Error while contacting the Scryfall API (Sets).');
+
+            return Command::FAILURE;
+        }
+
+        $sets = $response->collect($key = null)['data'];
+
+        foreach ($sets as $set) {
+            $set['scryfall_id'] = $set['id'];
+            unset($set['id']);
+
+            dd($set);
+        }
+
+        return 0;
+    }
+}
--- app/Models/Set.php
+++ app/Models/Set.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Card extends Model
+class Set extends Model
 {
     use HasFactory;
     use SoftDeletes;
