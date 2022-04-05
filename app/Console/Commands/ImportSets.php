3f36e751ad6070d03aea4d5f1d0e30f342b165b5
Nicolas Leroy <nicoleroymultimedia@gmail.com>
2022-04-05 16:23:18 +0200

    feature/card-model ok

--- app/Console/Commands/ImportSets.php
+++ app/Console/Commands/ImportSets.php
@@ -45,13 +45,29 @@ class ImportSets extends Command
 
             $entry = new Set();
 
+            $entry->scryfall_id = $set['scryfall_id'];
+            $entry->code = $set['code'];
+            $entry->mtgo_code = $set['mtgo_code'] ?? null;
+            $entry->tcgplayer_id = $set['tcgplayer_id'] ?? null;
             $entry->name = $set['name'];
+            $entry->set_type = $set['set_type'];
+            $entry->released_at = $set['released_at'] ?? null;
+            $entry->block_code = $set['block_code'] ?? null;
+            $entry->block = $set['block'] ?? null;
+            $entry->parent_set_code = $set['parent_set_code'] ?? null;
+            $entry->card_count = $set['card_count'];
+            $entry->printed_size = $set['printed_size'] ?? null;
+            $entry->digital = $set['digital'];
+            $entry->foil_only = $set['foil_only'];
+            $entry->nonfoil_only = $set['nonfoil_only'];
+            $entry->scryfall_uri = $set['scryfall_uri'];
+            $entry->uri = $set['uri'];
+            $entry->icon_svg_uri = $set['icon_svg_uri'];
+            $entry->search_uri = $set['search_uri'];
 
             $entry->save();
-
-
-
-            dd($set);
         }
+
+        return Command::SUCCESS;
     }
 }
